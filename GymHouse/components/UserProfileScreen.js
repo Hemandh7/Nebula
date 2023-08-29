4d57e7c90c037b0ba24c738ee764a0160fdf64a8
Hemandh <hemandh007bond@gmail.com>
2023-08-29 16:16:31 +0530

    initial form

--- GymHouse/components/UserProfileScreen.js
+++ GymHouse/components/UserProfileScreen.js
@@ -1,11 +1,34 @@
 import React, { useState } from "react";
 import { View, Text, TextInput, Pressable } from "react-native";
+import RNPickerSelect from 'react-native-picker-select';
 import styles from "./stylesheet";
+
 
 const UserProfileScreen = () => {
   const [name, setName] = useState("");
   const [age, setAge] = useState("");
   const [gender, setGender] = useState("");
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
+  const [email, setEmail] = useState("");
+  const [contactNumber, setContactNumber] = useState("+91");
+
+  const handleSaveProfile = () => {
+    const userProfile = {
+      name,
+      age,
+      gender,
+      height,
+      weight,
+      email,
+      contact_number: contactNumber, // Match the field name in your schema
+    };
+
+    // Here you can perform actions like sending the data to an API or saving it locally
+    console.log("User Profile:", userProfile);
+  };
+
+  
 
   return (
     <View style={styles.container}>
@@ -28,10 +51,33 @@ const UserProfileScreen = () => {
         value={gender}
         onChangeText={setGender}
       />
+    <TextInput
+        style={styles.input}
+        placeholder="Height"
+        value={height}
+        onChangeText={setHeight}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Weight"
+        value={weight}
+        onChangeText={setWeight}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Contact Number"
+        value={contactNumber}
+        onChangeText={setContactNumber}
+      />
       <Pressable
-        title="Save"
         style={styles.button}
-        onPress={() => console.log("Save profile")}
+        onPress={handleSaveProfile}
       >
         <Text style={styles.buttontext}>Save</Text>
       </Pressable>
